054ac3b362b160dbc4407daa95d9fce23b802782
Gahee Heo <ghbooth12@gmail.com>
2016-08-10 17:29:24 -0500

    3-refactor: completed

--- scripts/services/Movie.js
+++ scripts/services/Movie.js
@@ -7,20 +7,22 @@
     var ref = new Firebase('https://moviebook-58adb.firebaseio.com');
     var movies = $firebaseArray(ref.child('movies'));
 
+    function add(movie) {
+      if (movie.title && movie.rating && movie.review) {
+        movies.$add({
+          title: movie.title,
+          rating: movie.rating,
+          review: movie.review,
+          memo: movie.memo || " ",
+          createdAt: new Date().getTime()
+        });
+        this.movie = {};
+      }
+    }
+
     var obj = {
       all: movies,
-      add: function(movie) {
-        if (movie.title && movie.rating && movie.review) {
-          movies.$add({
-            title: movie.title,
-            rating: movie.rating,
-            review: movie.review,
-            memo: movie.memo,
-            createdAt: new Date().getTime()
-          });
-          this.movie = {};
-        }
-      }
+      add: add
     };
 
     return obj;
