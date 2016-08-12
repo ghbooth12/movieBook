3cce9e1d7e027006dc2a1151bce04281d34602dc
Gahee Heo <ghbooth12@gmail.com>
2016-08-12 14:49:18 -0500

    Add new properties for movie entry and show error msg when no result found

--- app/scripts/services/Poster.js
+++ app/scripts/services/Poster.js
@@ -4,28 +4,35 @@
     .factory('Poster', ['$http', Poster]);
 
   function Poster($http) {
+    var poster = {};
+    var base = 'http://api.themoviedb.org/3';
+    var service = '/search/movie';
+    var apiKey = '75eb3f0904ccb7d88cdb1b78a29f23b3';
+    var callback = 'JSON_CALLBACK'; // provided by angular.js
 
-    function search(film) {
+    poster.search = function(film) {
       var img = document.getElementById('movie-poster');
+      var errMsg = document.getElementById('err-msg');
       var output = 'http://image.tmdb.org/t/p/w500';
 
-      var base = 'http://api.themoviedb.org/3';
-      var service = '/search/movie';
-      var apiKey = '75eb3f0904ccb7d88cdb1b78a29f23b3';
-      var callback = 'JSON_CALLBACK'; // provided by angular.js
       var url = base + service + '?api_key=' + apiKey + '&query=' + film + '&callback=' + callback;
 
       $http.jsonp(url).then(function(data, status) {
-        output += data.data.results[0].poster_path;
-        img.src = output;
+        // console.log(JSON.stringify(data));
+        if (data.data.total_results === 0) {
+          errMsg.style.display = "block";
+        } else {
+          errMsg.style.display = "none";
+          output += data.data.results[0].poster_path;
+          img.src = output;
+          img.alt = data.data.results[0].original_title;
+        }
       }, function(data, status) {
-        // var json = JSON.stringify(data);
+        var json = JSON.stringify(data);
         console.log("ERROR", json, status);
       });
-    };
+    }
 
-    return {
-      search: search
-    };
+    return poster;
   }
 })();
