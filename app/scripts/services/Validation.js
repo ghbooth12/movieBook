61c2e18271532c7afe46ac1664be048af057c793
Gahee Heo <ghbooth12@gmail.com>
2016-08-15 16:42:45 -0500

    fixed validation and rate with stars

--- app/scripts/services/Validation.js
+++ app/scripts/services/Validation.js
@@ -6,31 +6,40 @@
   function Validation() {
     var validation = {};
 
-    validation.validTitle = function(length) {
-      if (length < 2) {
-        return 'short';
-      } else if (length > 100) {
-        return 'long';
-      } else {
-        return 'valid';
+    validation.validTitle = function(string) {
+      if (string) {
+        var length = string.length;
+        if (length < 2) {
+          return 'short';
+        } else if (length > 100) {
+          return 'long';
+        } else if (length >= 2 && length <= 100) {
+          return 'valid';
+        }
       }
     };
 
-    validation.validGenre = function(length) {
-      if (length > 100) {
-        return 'long';
-      } else {
-        return 'valid';
+    validation.validGenre = function(string) {
+      if (string) {
+        var length = string.length;
+        if (length > 100) {
+          return 'long';
+        } else {
+          return 'valid';
+        }
       }
     };
 
-    validation.validReview = function(length) {
-      if (length < 20) {
-        return 'short';
-      } else if (length > 100) {
-        return 'long';
-      } else {
-        return 'valid';
+    validation.validReview = function(string) {
+      if (string) {
+        var length = string.length;
+        if (length < 20) {
+          return 'short';
+        } else if (length > 100) {
+          return 'long';
+        } else if (length >= 20 && length <= 100) {
+          return 'valid';
+        }
       }
     };
 
